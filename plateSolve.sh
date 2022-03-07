f632395a46c674523b215e3a5cc1617ba53a2a46
Marek <456360@mail.muni.cz>
2022-03-07 16:46:24 +0100

    corrected calculations of ra and dec

--- plateSolve.sh
+++ plateSolve.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-echo "Target RA"
-read targetRA
-echo "Target Dec"
-read targetDec
+echo "Target RA, hh mm ss (eg. 13 40 32)"
+read RA
+echo "Target Dec, ° ' '' (eg -5 12 34)"
+read Dec
+
+ra1=$(echo $RA | cut -f1 -d' ')
+ra2=$(echo $RA | cut -f2 -d' ')
+ra3=$(echo $RA | cut -f3 -d' ')
+targetRA=`echo "scale=4; $ra1*15+$ra2*15/60+$ra3*15/3600" | bc`
+
+dec1=$(echo $Dec | cut -f1 -d' ')
+dec2=$(echo $Dec | cut -f2 -d' ')
+dec3=$(echo $Dec | cut -f3 -d' ')
+
+if (($dec1 < 0 )) 
+then
+targetDec=`echo "scale=4; $dec1-$dec2/60-dec3/3600" | bc`
+else
+targetDec=`echo "scale=4; $dec1+$dec2/60+dec3/3600" | bc`
+fi
 
 mkdir solve
 solve-field --no-plots --scale-units degwidth --scale-low 1 --scale-high 2 --downsample 2 -D solve "$1" > solve/out.txt 
@@ -31,7 +47,7 @@ Dec=${line:$i:7}
 echo "current RA is" $RA
 echo "current dec is" $Dec
 echo ""
-echo "$targetRA $RA" | awk '{print "RA difference:" $1-$2}'
-echo "$Dec $targetDec" | awk '{print "Dec difference:" $1-$2}'
+echo "$targetRA $RA" | awk '{print "RA difference:" $1-$2 degrees}'
+echo "$Dec $targetDec" | awk '{print "Dec difference:" $1-$2 degrees}'
 
 rm -r solve
